51a73cd03a22ce0cc5002f4a94c5622982431959
Matt <matt@mattlauderdale.com>
2018-06-12 23:03:53 -0400

    initial setup and fix

--- views/redux/reducers.js
+++ views/redux/reducers.js
@@ -1,19 +1,21 @@
-"use strict";
+'use strict';
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+
+var _actions = require('./actions');
 
 function apps() {
   var state = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : { isFetching: false, apps: [] };
   var action = arguments[1];
 
   switch (action.type) {
-    case REQUEST_APPS:
+    case _actions.REQUEST_APPS:
       return Object.assign({}, state, {
         isFetching: true
       });
-    case RECEIVE_APPS:
+    case _actions.RECEIVE_APPS:
       return Object.assign({}, state, {
         isFetching: false,
         apps: action.apps
